739693541fd55f5642b99cb7b5663e846f9c1dea
Dsys1129 <dsys1129@gmail.com>
2024-07-26 11:51:27 +0900

    fix: 통증 기록 메인 페이지 조회 시 사용자이름도 포함하도록 변경

--- src/main/java/com/intern_project/record/dto/response/RecordGroupResponseDTO.java
+++ src/main/java/com/intern_project/record/dto/response/RecordGroupResponseDTO.java
@@ -7,11 +7,13 @@ import java.util.List;
 
 @Getter
 public class RecordGroupResponseDTO {
+    private String username;
     private Integer totalCount;
     private Integer daysSinceLastRecord;
     private List<RecordGroupListResponseDTO> groups = new ArrayList<>();
 
-    public RecordGroupResponseDTO(Integer totalCount, Integer daysSinceLastRecord) {
+    public RecordGroupResponseDTO(String username, Integer totalCount, Integer daysSinceLastRecord) {
+        this.username = username;
         this.totalCount = totalCount;
         this.daysSinceLastRecord = daysSinceLastRecord;
     }
