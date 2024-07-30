a182a8b285e301498f886916b125ad5d039f8619
ydh0318 <edwinuo3@gmail.com>
2024-07-30 14:12:14 +0900

    비밀번호 암호화 추가

--- src/main/java/com/intern_project/config/SecurityConfig.java
+++ src/main/java/com/intern_project/config/SecurityConfig.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -34,5 +36,9 @@ public class SecurityConfig {
         return http.build();
     }
 
-
+    //암호화
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
